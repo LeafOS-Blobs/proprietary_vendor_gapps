212c71ba6e9b1b9be4921f7698dd0cc30e7ecd07
Tim Zimmermann <tim@linux4.de>
2022-06-15 21:16:08 +0200

    gapps: Initial commit
    * Imported from https://gitlab.com/MindTheGapps/vendor_gapps/-/tree/f0fd925e37e7c8acb32feea769d43d03de2d332e
    
    Change-Id: Ie2cd2133fd700fc81e7d5c0def551233dd0a91bb

---
+++ build/gms_module.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2022 The LeafOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifeq ($(LOCAL_MODULE_CLASS), APPS)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+endif
+ifeq ($(LOCAL_MODULE_CLASS), JAVA_LIBRARIES)
+LOCAL_SRC_FILES := $(LOCAL_MODULE).jar
+endif
+
+LOCAL_ENFORCE_USES_LIBRARIES := false
+
+ifneq (,$(wildcard vendor/gapps/prebuilt/common/$(LOCAL_SRC_FILES)*))
+LOCAL_SRC_FILES := $(wildcard vendor/gapps/prebuilt/common/$(LOCAL_SRC_FILES)*)
+else
+LOCAL_SRC_FILES := $(wildcard vendor/gapps/prebuilt/$(TARGET_ARCH)/$(LOCAL_SRC_FILES)*)
+endif
+LOCAL_SRC_FILES := $(shell echo $(LOCAL_SRC_FILES) | sed 's|^vendor/gapps/||g')
+
+ifneq (,$(filter %.xz.001,$(LOCAL_SRC_FILES)))
+include vendor/leaf/build/core/7z_prebuilt.mk
+else
+include $(BUILD_PREBUILT)
+endif
---
+++ gms.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2022 The LeafOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Apps
+PRODUCT_PACKAGES += \
+    GmsCore \
+    GooglePartnerSetup \
+    GoogleRestore \
+    GoogleServicesFramework \
+    Phonesky \
+    SetupWizard
+
+## FS Verity
+PRODUCT_COPY_FILES += \
+    vendor/gapps/etc/security/fsverity/gms_fsverity_cert.der:$(TARGET_COPY_OUT_PRODUCT)/etc/security/fsverity/gms_fsverity_cert.der
+
+## Libraries
+PRODUCT_PACKAGES += \
+    com.google.android.dialer.support
+
+## Overlays
+PRODUCT_PACKAGES += \
+    GmsOverlay
+
+## Permissions
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.xml,vendor/gapps/etc/default-permissions,$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions) \
+    $(call find-copy-subdir-files,*.xml,vendor/gapps/etc/permissions/product,$(TARGET_COPY_OUT_PRODUCT)/etc/permissions) \
+    $(call find-copy-subdir-files,*.xml,vendor/gapps/etc/permissions/system_ext,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions)
+
+## Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/gapps/overlay
+
+## Sysconfig
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.xml,vendor/gapps/etc/sysconfig,$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig)
